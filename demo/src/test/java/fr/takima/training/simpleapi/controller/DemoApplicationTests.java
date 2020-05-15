99f43c65cfe92ff289ad09aa4cb619350839e6f7
RemHD <ritchie.elloumi@gmail.com>
2020-05-15 17:47:28 +0200

    Initial commit

---
+++ demo/src/test/java/fr/takima/training/simpleapi/controller/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.takima.training.simpleapi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
